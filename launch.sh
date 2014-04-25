252ff579689d2ab9c2f6dff126f63a76eacffe7f
Etienne Andre <Etienne.Andre@univ-paris13.fr>
2014-04-25 19:24:22 +0000

    continue to simplify the debug PaTATOR
    git-svn-id: https://svn.lsv.ens-cachan.fr/kuehne/projects/IMITATOR2/branches/PaTATOR_debug@856 6f1578d2-a1e8-4f7d-a79a-10a3a1991f00

--- launch.sh
+++ launch.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-#################################################################
- #
- #                       BUILDATOR
- # 
- # Small script to build IMITATOR
- #
- # Universite Paris 13, Sorbonne Paris Cite, LIPN (France)
- # 
- # Author:        Etienne Andre
- # 
- # Created:       2014/01/13
- # Last modified: 2014/01/13
- #
-################################################################
-
 mpiexec -n 4 bin/PaTATOR
