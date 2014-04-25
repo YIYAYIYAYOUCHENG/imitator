5de4f4e66ffac2d7d311a01887662eb34d832bec
Etienne Andre <Etienne.Andre@univ-paris13.fr>
2014-04-25 19:17:40 +0000

    forgot one file
    git-svn-id: https://svn.lsv.ens-cachan.fr/kuehne/projects/IMITATOR2/branches/PaTATOR_debug@853 6f1578d2-a1e8-4f7d-a79a-10a3a1991f00

---
+++ launch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#################################################################
+ #
+ #                       BUILDATOR
+ # 
+ # Small script to build IMITATOR
+ #
+ # Universite Paris 13, Sorbonne Paris Cite, LIPN (France)
+ # 
+ # Author:        Etienne Andre
+ # 
+ # Created:       2014/01/13
+ # Last modified: 2014/01/13
+ #
+################################################################
+
+mpiexec -n 4 bin/PaTATOR ex/flipflop.imi ex/flipflop.v0 -mode cover -distributed -cart -verbose mediumvsss
